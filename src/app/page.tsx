b754aa3b61144258fe48244d347e7c6a171a6969
Wesley Coetzee <coetzeewr@gmail.com>
2024-01-03 12:22:00 +0100

    feat: Add basic page

---
+++ src/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <main className="p-5"></main>;
+}
---
+++ src/components/Footer.tsx
@@ -0,0 +1,29 @@
+export default function Footer() {
+  return (
+    <footer className="fixed bottom-0 left-0 p-4 flex items-end w-full flex-col">
+      <div className="flex flex-row gap-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+          />
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+          />
+        </svg>
+
+        <span>The Hague, Netherlands</span>
+      </div>
+    </footer>
+  );
+}
