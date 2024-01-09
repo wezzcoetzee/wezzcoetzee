d4dd50c29174b7166f925907986c2184b7e27eeb
Wesley Coetzee <coetzeewr@gmail.com>
2024-01-09 07:34:35 +0100

    feat: Add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Script
+          strategy="lazyOnload"
+          src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"
+        />
+        <Script strategy="lazyOnload" id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-M18S2XK0BZ');
+        `}
+        </Script>
         <div className="bg-slate-200 h-screen overflow-hidden flex justify-center items-center w-full tracking-tighter">
           {children}
           <Header />
